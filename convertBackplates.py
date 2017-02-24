66eadc92e9aeb3c9e31ce460fcf334267a7cc3d3
flapcats <flapcats@gmail.com>
2017-02-24 10:43:15 +0000

    Convert plates to linear for Arnold render view.
    Converts backplates to linear for Arnold render view, drops them in a 'linear' folder.

---
+++ convertBackplates.py
@@ -0,0 +1,37 @@
+import os.path
+import glob
+import maya.cmds as cmds
+import os
+import maya.mel as mel
+import sys
+import subprocess
+
+#Gets the start and end frames of a sequence (thanks Will!)
+def dpxSeq():
+    filePath = cmds.fileDialog(title="Convert PULL DPX > EXR")
+    filename, file_extension = os.path.splitext(filePath)
+    file_list = glob.glob(filename[:-4] + "*" + file_extension) # create filtered list of files
+    
+    frames = [f[-8:-4] for f in file_list]
+    print min(frames), max(frames)
+    
+    #makes a "/linear" directory
+    withHash = (filename[:-4] + "####" + file_extension)
+    folderPath, fullFilename = os.path.split(withHash)
+    linearPath = folderPath+"/linear/"
+    exportPath = linearPath+fullFilename
+    exportFilename = (exportPath[:-4]+".exr")
+    print exportFilename
+    
+    if not os.path.isdir( folderPath ):
+        os.makedir( folderPath )
+        print "Created dir " + folderPath
+    
+    #The command for a shell:
+    sillyString = "/mnt/pipeline/software/Nuke9.0v4/Nuke9.0 -x /mnt/resources/nuke/lightingBackplates/lightingBackplates_v004.nk {} {} {}-{}".format(withHash, exportFilename, min(frames), max(frames))
+    print sillyString
+
+    return sillyString
+
+def convertBackplates():
+    subprocess.Popen(dpxSeq(), shell=True)
