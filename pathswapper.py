f5d089816c67f8f84519f6ea52206262b710921a
lookbothways <flapcats@gmail.com>
2020-02-29 17:47:41 +0000

    Update pathswapper.py

--- pathswapper.py
+++ pathswapper.py
@@ -1,4 +1,6 @@
 #Nuke tool to swap shot name and version for another
+#https://note.nkmk.me/en/python-str-replace-translate-re-sub/
+
 import re
 
 print "Pathswapper"
@@ -8,19 +10,28 @@ p.addFilenameSearch('Path to beauty', "//islay/projects/shangrila/publish/episod
 p.addButton('Cancel')
 p.addButton('Update all read nodes')
 ret = p.show()
+#test
+fullPath = ["things/thing/1030thingy/1030/thingles1030_v001.%20d.exr","things/thing/1030thingy/1030/thingles1030_v001.%20d.exr","things/thing/1030thingy/1030/thingles1030_v001.%20d.exr","things/thing/1030thingy/1030/thingles1030_v001.%20d.exr","things/thing/1030thingy/1030/thingles1030_v001.%20d.exr"]
 
 
-
-if 'beauty' in fullPath:
+if 'exr' in fullPath:
     fullPath = p.value('Path to beauty')[:-10]
     #print fullPath
 
-    shotNumber = fullPath.split('_sh')[4]
-    print shotNumber
+ 
+    oldShotNumber = "1030"
+    newShotNumber = "1160"
+    oldVersion = "001"
+    newVersion = "012"
 
+    for each in fullPath:
+        each = each.replace(str(oldShotNumber), str(newShotNumber))
+        each = each.replace(str(oldVersion), str(newVersion))
+        print(each)
+        #nuke.set knob value here
 
 
 else:
-    print "File format error please choose a beauty render"
+    print "File format error please choose an exr to switch paths to"
 
 
