19f2c740eb0128af9148260a1523c3d9a7c56fa8
lookbothways <flapcats@gmail.com>
2020-03-03 15:51:32 +0000

    Update pathswapper.py

--- pathswapper.py
+++ pathswapper.py
@@ -1,38 +1,53 @@
-#Nuke tool to swap shot name and version for another
-#https://note.nkmk.me/en/python-str-replace-translate-re-sub/
-#note this code is WIP and specific to a project
+#//islay/projects/shangrila/publish/episodes/ep101_banished/ep101_sh1130/Lighting/ep101_sh1130_Lighting_v012/images/char_beauty/
 
-import re
+newShotNumber = ""
+newVersion = ""
+fullPath = ""
+allReads = []
+each = []
 
-print "Pathswapper"
 #Get path to new sequence
 p = nuke.Panel('Pathswapper')
-p.addFilenameSearch('Path to beauty', "//islay/projects/shangrila/publish/episodes/")
+p.addFilenameSearch('Path to new seq', "//islay/projects/shangrila/publish/episodes/ep101_banished/ep101_sh1130/Lighting/ep101_sh1130_Lighting_v012/images/char_beauty/")
 p.addButton('Cancel')
 p.addButton('Update all read nodes')
 ret = p.show()
-#test
-fullPath = ["things/thing/1030thingy/1030/thingles1030_v001.%20d.exr","things/thing/1030thingy/1030/thingles1030_v001.%20d.exr","things/thing/1030thingy/1030/thingles1030_v001.%20d.exr","things/thing/1030thingy/1030/thingles1030_v001.%20d.exr","things/thing/1030thingy/1030/thingles1030_v001.%20d.exr"]
 
+fullPath = str(p.value('Path to beauty')[:-10])
+newShotNumber = str(fullPath.split('_sh')[2])[:4]
+newVersion = str(fullPath.split('_v')[2])[:3]
+print "Updating all read nodes to shot "+ str(newShotNumber) +" v"+ str(newVersion)
 
-if 'exr' in fullPath:
-    fullPath = p.value('Path to beauty')[:-10]
-    #print fullPath
+for a in nuke.allNodes():
+    if 'FILEPATH_BG' in a['name'].value():
+        print "Hello"
+
+for a in nuke.allNodes():
+    if 'Read' in a['name'].value():
+        if 'Lighting' in a['file'].value():
+            file = str(a.knob('file').getValue())
+            oldShotNumber = str(file.split('_sh')[2])[:4] 
+            oldVersion = str(file.split('_v')[2])[:3]
+            file = file.replace(str(oldShotNumber), str(newShotNumber))
+            file = file.replace(str(oldVersion), str(newVersion))
+            #Apply paths
+            newFile = a.knob('file').setValue(file)
+            print str(file)
+
+for a in nuke.allNodes():
+    if 'FILEPATH_BG' in a['name'].value():
+        print "FILEPATH_BG found"
+        file = str(a.knob('label').getValue())
+        print file
+        oldShotNumber = str(file.split('_sh')[2])[:4] 
+        oldVersion = str(file.split('_v')[1])[:3]
+        file = file.replace(str(oldShotNumber), str(newShotNumber))
+        file = file.replace(str(oldVersion), str(newVersion))
+        #Apply paths
+        newFile = a.knob('label').setValue(file)
+        print str(file)
+
+#To Do - put it all in a module with arguements instead of brute-forcing the nuke.allNodes() file / label split-position setup as above.
 
  
-    oldShotNumber = "1030"
-    newShotNumber = "1160"
-    oldVersion = "001"
-    newVersion = "012"
-
-    for each in fullPath:
-        each = each.replace(str(oldShotNumber), str(newShotNumber))
-        each = each.replace(str(oldVersion), str(newVersion))
-        print(each)
-        #nuke.set knob value here
-
-
-else:
-    print "File format error please choose an exr to switch paths to"
-
 
